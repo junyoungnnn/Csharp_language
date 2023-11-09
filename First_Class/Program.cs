2a962873b0c619b2a454952dc151f51155e0a0de
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-11-09 19:46:38 +0900

    (2023.11.09) 매개변수 한정자

---
+++ First_Class/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace First_Class
+{
+    // 접근 지정자 class 클래스 이름
+    // public, protected, private
+
+    // <자료형>
+    // Object 자료형
+
+    // 값타입과 참조 타입
+    // 값타입: bool, byte, char, short, int, long, float, double, struct, decimal, long long
+    // 참조타입: class, string, delegate, array, dynamic, object, interface (참조타입이 값타임보다 더 큰 개념)
+
+    // Boxing
+    // 값타입 -> 참조타입
+    // Unboxing
+    // 참조타입 -> 값타입
+
+    // 배열 생성
+    // int [] buffer = new int[5];
+    // 스택에 buffer참조 변수가 생기고
+    // 힙에 메모리가 5개가 생김
+    // delete 할 필요없이 가비지 컬렉터가 자동으로 삭제함
+
+    // 세대 기반 가비지 컬렉션 (압축 기능) // 자투리 공간 활용
+    // 비세대 기반 가비지 컬렉션 (압축 기능 X)\
+
+    // c#에서는 자료형 크기 보는것이 막혀있음
+
+    public class Module
+    {
+        public void SetData()
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 자료형
+            /*
+            // 초기화를 안하면 자동으로 0값이 들어감
+            byte stream = 1;
+            bool boolean = false;
+            char character = 'A';
+            short data = 200;
+            int integer = 100;
+            long longData = 300;
+
+            float[] buffer = new float[3];
+            buffer[0] = 1.15f;
+            // c#에서 float를 사용하려면 f접미사를 꼭 붙여야함 3.14f
+            double pi = 3.14;
+            decimal decimalData = 0.1m; // 16 byte, 접미사 m
+
+            // 출력해주는 함수
+            // Console.WriteLine(): 문자열을 출력하는 함수
+            Console.WriteLine(boolean);
+            Console.Write("steam " + stream);
+            // 개행 하기 싫으면 Console.Write()을 사용
+            Console.WriteLine("stream " + stream);
+            */
+            #endregion
+
+
+            // 박싱과 언박싱은 속도가 엄청 느림
+            #region 박싱(Boxing)
+            // 값 형식의 데이터를 참조 형식으로 변환하는 과정입니다.
+
+            //int data = 5;
+
+            //object obj = data;
+            //Console.WriteLine("data의 값: " + data);
+            //Console.WriteLine("obj의 값: " + obj);
+
+            #endregion
+
+            #region 언박싱(UnBoxing)
+            /*
+            // 참조 타입의 데이터를 값 타입으로 변환하는 과정입니다.
+
+            int count = 10;
+            object obj1 = count;
+
+            //참조타입이 더 큰데 작은 값에 넣으려 하니 ERROR
+            // int result = obj1; ERROR
+
+            // 명시적으로 값 변환을 해주어야함
+            int result = (int)obj1;
+            */
+            #endregion
+
+        }
+    }
+}
---
+++ Second_Class/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace Second_Class
+{
+    public class Item
+    {
+        private int price;
+        public string name;
+
+        public void View()
+        {
+            Console.WriteLine("name: " + name);
+            Console.WriteLine("price: " + price);
+        }
+    }
+
+    public class Vector3
+    {
+        // ref: 하나의 변수에 서로 다른 이름으로 사용할 수 있는 키워드 입니다.
+        public void Distance(ref int x)
+        {
+            x = 100;
+        }
+
+        // in: const ref라고 생각하면됨
+        // 읽기 전용으로 적용되며, 참조로 전달하는 키워드입니다.
+        public void Lerp(in int x)
+        {
+            Console.WriteLine("x의 값: " + x);
+        }
+
+        // out : 출력용 매개 변수로 함수 내에서 값을 반드시 초기화해야 합니다.
+        public void MoveTowards(out int x)
+        {
+            x = 999;
+        }
+
+        // params : 가변 길이 매개변수
+        public void Normalize(params int[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                int sum = 0;
+                sum += (int)list[i];
+                Console.Write(sum + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 가비지 컬렉터
+            /*
+            // 동적으로 할당된 메모리 영역 가운데 더 이상
+            // 사용할 수 없게 된 영역을 탐지하여 자동으로 해제하는 기법입니다.
+            {
+                // 가비지 컬렉션이 자동으로 해제하기때문에 수동으로 delete 할 필요없음
+                Item item = new Item();
+                item.View();
+            }
+
+            // 참조변수 Item item이 사라져서 사용을 못함
+            Item potion = new Item();
+
+            // <세대별 가비지 컬렉션>
+            // 0세대 가장 빈번하게 삭제되는 애들이 있는 곳
+            // 1세대 0세대 가비지 컬렉션이 실행되고 남은 애들이 옴
+            // 2세대 1세대 가비지 컬렉션이 실행되고 남은 애들과 계속 남아있는 애들
+            // FULL GC가 잘 안생기게 해주어야함
+            */
+            #endregion
+
+            #region 매개변수 한정자
+
+            int x = 0;
+
+            Vector3 vector3 = new Vector3();
+            // ref
+            /*
+            vector3.Distance(ref x);
+
+            Console.WriteLine("x의 값: " + x);
+            */
+
+            // in 
+            /*
+            vector3.Lerp(x);
+           */
+
+            // out
+            /*
+            vector3.MoveTowards(out x);
+            Console.WriteLine("x의 값: " + x);
+            */
+
+            // params
+            /*
+            vector3.Normalize(1, 2, 3, 4, 5);
+            vector3.Normalize('A', 'B', 'C', 'D', 'E');
+            */
+            #endregion
+        }
+    }
+}
