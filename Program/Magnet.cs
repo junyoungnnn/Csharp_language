4cb1aa4752aa45fd314f4de8bb635487512652c4
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-11-13 19:47:42 +0900

    (2023.11.13) 인터페이스와 추상클래스

---
+++ Program/Magnet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class Magnet : IItem, IWaitTime
+    {
+        public void wait(float timer)
+        {
+            Console.WriteLine("wait Time: " + timer);
+        }
+
+        public void Use()
+        {
+            Console.WriteLine("Use Magnet");
+        }
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,146 +1,107 @@
 ﻿namespace Program
 {
-
-    #region 프로퍼티
-    public class Transform
+    public interface IItem
     {
-        private int x;
-        private int y;
-        private int z;
-
-        public int X
-        {   
-            set 
-            {   if(value >= 100)
-                {
-                    Console.WriteLine("I received an unexpected value");
-                    return;
-                }
-
-                x = value; 
-            }
-            get { return x; } // 읽기 전용, X를 반환하면 자기 자신을 여러번 호출하기 때문에
-                              // 무한 반복이 된다.
-        }
-    }
-    #endregion
-
-    #region 메소드 숨기기
-    public class Weapon
-    {
-        protected int attack;
-
-        public void Information()
-        {
-            Console.WriteLine("Weapon 정보");
-        }
-
-        public virtual void Action()
-        {
-            Console.WriteLine("Weapon의 동작");
-        }
+        void Use();
     }
 
-    public class Sword : Weapon
+    public interface IWaitTime
     {
-        public new void Information()
-        {
-            Console.WriteLine("Sword 정보");
-        }
-
-        public override void Action()
-        {
-            Console.WriteLine("Sword의 동작");
-        }
+        void wait(float timer);
     }
 
+    public class Player
+    {
+        public void OnTrigegerEnter(IItem item)
+        {
+            item.Use();
+        }
+        //public void OnTrigegerEnter(IWaitTime timer)
+        //{
+        //    timer.wait(50.0f);
+        //}
 
 
-    #endregion
+    }
 
-    #region 함수의 오버로딩
     public class GameObject
     {
-        public void Destory()
+        private int guID;
+        public int GUID {
+            set { guID = value; }
+            get { return guID; } 
+        }
+
+        public GameObject Clone()
         {
-            Console.WriteLine("Destory GameObject");
-        }
+            // 1.class를 생성합니다.
+            GameObject cloneObject = new GameObject();
 
-        public void Destory(float timer)
-        { 
-            Console.WriteLine("Destory GameObject " + "Destory Time: " + timer);
-        }
+            // 2. class 안에 있는 데이터를 넣어줍니다.
+            cloneObject.guID = guID;
 
-        public void GetComponent(string name, int id)
-        {
-            Console.WriteLine("name : " + name);
-            Console.WriteLine("id : " + id);
+            // 3. class를 반환합니다.
+            return cloneObject;
         }
-
-        public void SetActive(bool active = true)
-        { 
-            Console.WriteLine("GameObject state : " + active);
-        }
-
     }
-
-
-
-    #endregion
+  
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region 프로퍼티
-            // 프로퍼티
+            #region 얕은 복사
             /*
-            Transform transform = new Transform();
-            transform.X = 100;
-            Console.WriteLine(transform.X);
+            GameObject gameObject1 = new GameObject();
+
+            GameObject gameObject2 = gameObject1;
+
+            gameObject2.GUID = 99;
+
+            Console.WriteLine("gameObject1의 guid: " +  gameObject1.GUID);
+            Console.WriteLine("gameObject2의 guid: " +  gameObject2.GUID);
             */
             #endregion
 
-            #region 매소드 숨기기
-            // 메소드 숨기기
-            /*
-            Sword sword = new Sword();
-            sword.Information();
-            sword.Action();
-            */
+            #region 깊은 복사
+
+            //GameObject clone1 = new GameObject();
+
+            //GameObject clone2 = clone1.Clone();
+
+            //clone1.GUID = 456;
+            //clone2.GUID = 111;
+
+            //Console.WriteLine("clone1의 guid: " + clone1.GUID);
+            //Console.WriteLine("clone2의 guid: " + clone2.GUID);
+
             #endregion
 
-            #region 가상함수
-            // 가상 함수
-            /*
-            Weapon weapon = new Sword(); // Weapon 타입이라 Weapon만 읽을 수 있음
-            weapon.Information(); // => Weapon의 정보
-            weapon.Action();      // => Sword의 동작, virtual로 정의, vptr을 사용해서 가상함수 테이블에서 호출
-            */
+            // C#은 다중상속을 지원하지 않습니다.
+            // 인터페이스는 다중상속을 지원합니다.
+            // public interface를 만들어서 상속시키면 된다.
+            // interface 뒤에 'I'를 꼭 붙여주어야한다.
+            // interface 안에 있는 함수는 상속한 클래스에서 재정의를 해주어야한다.
+
+            #region 인터페이스
+            Player player = new Player();
+            
+            // 인터페이스 참조 변수를 선언할 수 있지만,
+            // 인스턴스를 생성할 수 없습니다.
+            // IItem item = new IItem();
+
+            player.OnTrigegerEnter(new Magnet());
+            player.OnTrigegerEnter(new Missile());
+            player.OnTrigegerEnter(new Shield());
+
             #endregion
 
-            #region 함수의 오버로딩
-            // 함수의 오버로딩
-            GameObject gameObject = new GameObject();
-            /*
-            gameObject.Destory();
-            gameObject.Destory(5f);
-            */
-            #endregion
+            #region 추상클래스
 
-            #region 명몀된 매개변수
-            // 명명된 매개변수
-            // gameObject.GetComponent(id: 5, name: "Sphere");
-            // gameObject.GetComponent("Box", 10);
-            #endregion
+            Juggling juggling = new Juggling();
+            juggling.Information();
+            juggling.Skill();
 
-            #region 선택적 매개변수
-            // 선택적 매개변수 == 디폴트 매개변수
-            /*
-            gameObject.SetActive();
-            gameObject.SetActive(false);
-            */
             #endregion
-
         }
     }
 }
