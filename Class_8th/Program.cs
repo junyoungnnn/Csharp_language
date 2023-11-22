33984368dfe8024a0117d2e6a2afbe7b916e6f21
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-11-22 23:52:23 +0900

    공부중

--- Class_8th/Program.cs
+++ Class_8th/Program.cs
@@ -162,6 +162,43 @@
             }
             */
 
+            // HashTable
+            /*
+            Hashtable hashTable = new Hashtable();
+
+            // KEY - VALUE
+            hashTable.Add("Sword", 1000);
+            hashTable.Add('A', 57.4f);
+            hashTable.Add(203, 74.123);
+
+            // 구조체는 값 타입
+            // 클래스는 참조 타입
+
+            foreach(DictionaryEntry pair in hashTable)
+            {
+                Console.WriteLine("Key : " + pair.Key + " " + "Value : " + pair.Value );
+            }
+            */
+
+            // Dictionary
+            /*
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            dictionary.Add("Maple story", 2003);
+            dictionary.Add("League of Legend", 2011);
+            dictionary.Add("Mini Fighter", 2006);
+
+            if(dictionary.ContainsKey("Maple story"))
+            {
+                Console.WriteLine("키가 존재합니다");
+            }
+
+            foreach(var element in dictionary)
+            {
+                Console.WriteLine(element);
+            }
+            */
+
             #endregion
         }
     }
--- Program/Program.cs
+++ Program/Program.cs
@@ -8,25 +8,44 @@ namespace Program
     internal class Program
     {
         
-
         static void Main(string[] args)
         {
-            // HashTable
-            Hashtable hashTable = new Hashtable();
-
-            // KEY - VALUE
-            hashTable.Add("Sword", 1000);
-            hashTable.Add('A', 57.4f);
-            hashTable.Add(203, 74.123);
-
-            // 구조체는 값 타입
-            // 클래스는 참조 타입
-
-            foreach(DictionaryEntry pair in hashTable)
+            #region 예외 처리
+            /*
+            try
             {
-                Console.WriteLine("Key : " + pair.Key + " " + "Value : " + pair.Value );
+                int data = 0;
+
+                Console.WriteLine(100 / data);
+            }
+            catch ( DivideByZeroException error)
+            {
+                Console.WriteLine(error.Message);
             }
 
+            try
+            {
+                int[] array = new int[3];
+
+                array[3] = 333;  // catch 쪽으로 jump
+
+                Console.WriteLine("프로그램 시작"); // 수행 안됨
+            }
+            catch(Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+            finally
+            {
+                Console.WriteLine("프로그램 종료");
+            }
+            */
+
+            #endregion
+
+            #region 열거자
+
+            #endregion
         }
     }
 }
