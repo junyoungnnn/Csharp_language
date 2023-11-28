d9efb313c79353aa90edc8592e53469e8c22d82f
park jin hee <jh750531@naver.com>
2023-11-28 19:47:18 +0900

    (2023.11.28) 인접 행렬

---
+++ Program/AdjacencyList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class AdjacencyList
+    {
+        // 그래프의 노드
+
+        // 그래프의 인접 리스트
+        private List<char>[] adjacencyList;
+
+        private int size;
+        private int edgeSize;
+
+        public AdjacencyList(int maxSize)
+        {
+            size = 0;
+            adjacencyList = new List<char>[maxSize];
+        }
+
+        public void AddVertex(char data)
+        {
+            size++;
+            adjacencyList[size++] = data;
+        }
+
+        public void AddEdge(List<char> adjacencyList)
+        {
+            edgeSize++;
+        }
+
+        public void Display()
+        {
+            for(int i = 0; i< size; i++)
+            {
+                for(int j = 0; j< edgeSize; j++)
+                {
+                    Console.Write(adjacencyList[j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
---
+++ Program/AdjacencyMatrix.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class AdjacencyMatrix <T>
+    {
+        // 속성
+        // 1. 정점의 수 (size)
+        // 2. 정점의 이름 (vertex)
+        // 3. 인접 행렬 (2차원 배열)
+
+        private int size;
+        private T[] vertex;
+        private int[,] arr;
+
+
+        public AdjacencyMatrix(int matrixSize)
+        {
+            size = 0;
+            arr = new int[matrixSize, matrixSize];
+            vertex = new T[matrixSize];
+        }
+
+        public void InsertVertex(T data)
+        {
+            vertex[size++] = data;
+        }
+
+        public void InsertEdge(int x, int y)
+        {
+            arr[x, y] = 1;
+            arr[y, x] = 1;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Vertex Size : " + size);
+
+            Console.WriteLine("");
+
+            for(int i =0; i<size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
